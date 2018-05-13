547668880cd6d7eba99951c3fe8dfa56e3091df2
Unknown <mikeyredtiger22@gmail.com>
2018-05-13 22:50:01 +0100

    Firebase auth and data validation completed.

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
-import {StyleSheet, View} from 'react-native'
-import {Container, Header, Title, Left, Icon, Right, Button, Body, Content, Text, Card, CardItem} from "native-base"
+import {StyleSheet} from 'react-native'
+import {Container, Header, Title, Button, Body, Content, Text, Form, Item, Input} from "native-base"
 import firebase from 'react-native-firebase'
 
 
@@ -8,28 +8,27 @@ export class LoginScreen extends React.Component {
   constructor() {
     super();
     this.state = {
-      loading: true,
       isAuthenticated: false,
-    };
+      user: null,
+      email: '',
+      emailError: '',
+      password: '',
+      passwordError: '',
+      text: '',
+    }
   }
 
   componentDidMount() {
-    // this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
-    //   this.setState({
-    //     loading: false,
-    //     user,
-    //   });
-    // });
-    firebase.auth().signInAnonymously()
-      .then(() => {
-        this.setState({
-          isAuthenticated: true,
-        });
+    this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        user: user,
       });
+      console.log('logged in')
+    })
   }
 
   componentWillUnmount() {
-    // this.authSubscription();
+    this.authSubscription()
   }
 
   static navigationOptions = ({navigation}) => ({
@@ -39,48 +38,115 @@ export class LoginScreen extends React.Component {
         <Title>Login</Title>
         </Body>
       </Header>
-    )
-  })
+    ),
+  });
+
+  log = (text) => {
+    this.setState({text: this.state.text + text})
+  };
+
+  logout = () => {
+    firebase.auth().signOut()
+  };
 
   render() {
+    let userID = null;
+    if (this.state.user) {
+      userID = this.state.user.uid
+    }
     return (
       <Container>
         <Content padder>
-          <Text style={{alignSelf: "center"}}>Login 1:30</Text>
-          <Text>{this.state.isAuthenticated}</Text>
-          <Button block success onPress={this.login()}>
+          <Text style={{alignSelf: "center"}}>Login</Text>
+          <Text>Auth: {userID}</Text>
+          <Form>
+            <Item rounded error={!!this.state.emailError}>
+              <Input
+                placeholder='Email'
+                keyboardType='email-address'
+                style={{marginHorizontal: 15}}
+                onBlur={() => this.validateEmail()}
+                onChangeText={(email) => this.setState({email: email})}
+              />
+            </Item>
+            <Item>
+              {!this.state.emailError ? null :
+                <Text style={{color: 'red'}}>{this.state.emailError}</Text>}
+            </Item>
+            <Item rounded error={!!this.state.passwordError}>
+              <Input
+                placeholder='Password'
+                secureTextEntry
+                style={{marginHorizontal: 15}}
+                onBlur={() => this.validatePassword()}
+                onChangeText={(password) => this.setState({password: password})}
+              />
+            </Item>
+            <Text>
+              {this.state.text}
+            </Text>
+            <Item>
+              {!this.state.passwordError ? null :
+                <Text style={{color: 'red'}}>{this.state.passwordError}</Text>}
+            </Item>
+          </Form>
+          <Button block success onPress={this.login} disabled={this.state.email.length === 0}>
             <Text>Login</Text>
           </Button>
-          <View>
-            <Text>The following Firebase modules are enabled:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>Admob</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>Analytics</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>Authentication</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>Crashlytics</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>Cloud Firestore</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>Cloud Messaging</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>Dynamic Links</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>Instance ID</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>Notifications</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>Performance Monitoring</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>Realtime Database</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>Remote Config</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>Storage</Text>}
-          </View>
+          <Button onPress={this.logout}><Text>Logout</Text></Button>
         </Content>
       </Container>
     )
   }
 
+  validateEmail = () => {
+    let email = this.state.email;
+    if (!email || email.length === 0) {
+      this.setState({emailError: 'Email required'});
+      return false
+    }
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(email)) {
+      this.setState({emailError: 'Not a valid email'});
+      return false
+    }
+    this.setState({emailError: ''});
+    return true
+  };
+
+  validatePassword = () => {
+    let password = this.state.password;
+    if (!password || password.length === 0) {
+      this.setState({passwordError: 'Password required'});
+      return false
+    }
+    if (password.length < 6) {
+      this.setState({passwordError: 'Password must be at least 6 characters'});
+      return false
+    }
+    this.setState({passwordError: ''});
+    return false
+  };
+
   login = () => {
+    let emailValid = this.validateEmail();
+    let passwordValid = this.validatePassword();
+    if (!(emailValid && passwordValid)) return;
+
+
     firebase.auth()
-      .signInAnonymouslyAndRetrieveData()
-      .then(credential => {
-        if (credential) {
-          console.log('default app user ->', credential.user.toJSON())
-        }
-      })
-  }
+    // .signInAnonymouslyAndRetrieveData()
+    .createUserAndRetrieveDataWithEmailAndPassword(this.state.email, this.state.password)
+    .then(credential => {
+      if (credential) {
+        console.log('defaaault app user ->', credential.user.toJSON())
+        this.props.navigation.navigate()
+      }
+    })
+    .catch(error => {
+      console.warn('error:', error)
+    })
+  };
 }
 
 const styles = StyleSheet.create({
@@ -88,7 +154,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
-})
+});
 
